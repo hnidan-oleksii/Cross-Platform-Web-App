c6d8e15af064b5fdbfb56829a9145e517409f240
surikadt <gnidanol@gmail.com>
2024-04-15 21:45:33 +0300

    [3] Added Tailwind UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme= require('tailwindcss/defaultTheme');
 module.exports = {
   content: ['./src/main/resources/templates/**/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      }
+    },
   },
-  plugins: [require('@tailwindcss/container-queries'),require('daisyui'),require('@tailwindcss/forms'),require('@tailwindcss/typography')],
+  plugins: [require('@tailwindcss/container-queries'),require('daisyui'),require(
+      '@tailwindcss/forms'),require('@tailwindcss/typography')],
 }
-
