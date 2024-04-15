8f697d9bc973444cd3d68fa34a2e73135f681fc5
surikadt <gnidanol@gmail.com>
2024-04-15 21:10:49 +0300

    [3] Initial commit

---
+++ src/test/java/edu/chnu/ua/crossplatform/CrossPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.chnu.ua.crossplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrossPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
