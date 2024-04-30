5313c7129e11a504c58e067d8f01de1b2e6b30d5
surikadt <gnidanol@gmail.com>
2024-04-30 15:07:19 +0300

    [4] Created parameterized fragment for menu items

---
+++ src/main/java/edu/chnu/ua/crossplatform/CrossPlatformApplicationConfiguration.java
@@ -0,0 +1,19 @@
+package edu.chnu.ua.crossplatform;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+@Configuration
+public class CrossPlatformApplicationConfiguration {
+
+    @Bean
+    public ITemplateResolver svgTemplateResolver() {
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setPrefix("classpath:/templates/svg/");
+        resolver.setSuffix(".svg");
+        resolver.setTemplateMode("XML");
+        return resolver;
+    }
+}
