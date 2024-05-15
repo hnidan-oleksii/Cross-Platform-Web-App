99a6b05f8011993116092c95b276930e1cdf42b8
surikadt <gnidanol@gmail.com>
2024-05-15 18:38:54 +0300

    [6][4] Fixed CrossPlatformApplicationTests by using test containers

--- src/test/java/edu/chnu/ua/crossplatform/CrossPlatformApplicationTests.java
+++ src/test/java/edu/chnu/ua/crossplatform/CrossPlatformApplicationTests.java
@@ -1,9 +1,13 @@
 package edu.chnu.ua.crossplatform;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("spring-boot-test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class CrossPlatformApplicationTests {
 
 	@Test
