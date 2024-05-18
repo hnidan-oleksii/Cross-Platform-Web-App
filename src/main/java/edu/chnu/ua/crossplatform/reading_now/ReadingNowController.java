0ef5467ca2ef69b2b22d101df8303216dd212686
surikadt <gnidanol@gmail.com>
2024-05-18 19:35:48 +0300

    [8][3] Extended controller to redirect to create form and back

--- src/main/java/edu/chnu/ua/crossplatform/reading_now/ReadingNowController.java
+++ src/main/java/edu/chnu/ua/crossplatform/reading_now/ReadingNowController.java
@@ -1,15 +1,21 @@
 package edu.chnu.ua.crossplatform.reading_now;
 
+import edu.chnu.ua.crossplatform.books.CreateBookFormData;
 import edu.chnu.ua.crossplatform.books.book.BookService;
+import edu.chnu.ua.crossplatform.books.book.Genre;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.SortDefault;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
-@RequestMapping("/")
+@RequestMapping(value={"/", "/reading_now"})
 public class ReadingNowController {
     private final BookService bookService;
 
@@ -23,5 +29,19 @@ public class ReadingNowController {
         model.addAttribute("activeMenuItem", "reading-now");
         model.addAttribute("books", bookService.getBooks(pageable));
         return "books/books";
+    }
+
+    @GetMapping("/create")
+    public String createBookForm(Model model) {
+        model.addAttribute("book", new CreateBookFormData());
+        model.addAttribute("genres", List.of(Genre.values()));
+        return "books/edit";
+    }
+
+    @PostMapping("/create")
+    public String createBook(@ModelAttribute("book") CreateBookFormData formData,
+                             Model model) {
+        bookService.createBook(formData.toParameters());
+        return "redirect:/";
     }
 }
